9740eaa9add797ee24d8e039e5966663853734cc
calint <calin.tenitchi@gmail.com>
2024-12-23 02:16:53 +0700

    .

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-g++ -std=c++23 -o osqa -Wfatal-errors -Werror -Wall -Wextra -Wpedantic -Wno-unused-parameter src/main.cpp
+g++ -std=c++23 -o osqa -fno-rtti -fno-exceptions -Wfatal-errors -Werror -Wall -Wextra -Wpedantic -Wno-unused-parameter src/main.cpp
 echo
 ls -la --color osqa
 echo
